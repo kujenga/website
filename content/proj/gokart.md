32f407fbe4eb0744530137479b4267e4e3d1c03d
kujenga <ataylor0123@gmail.com>
2015-06-17 23:35:04 -0400

    gulp config for more build config. further linting of html

--- content/proj/gokart.md
+++ content/proj/gokart.md
@@ -6,6 +6,6 @@ header_img = "/img/proj/gokart_wide.jpg"
 categories = ["homepage"]
 +++
 
-<img src="/img/proj/gokart_wide.jpg" alt="Go kart in action" width="80%">
+<img src="/img/proj/gokart_wide.jpg" alt="Go kart in action" width="800">
 
 Hand-fabricated a full-suspension metal frame from raw of steel tubing. Designed the electronics system for the power train, used CAD software to design the motor and control board mounts and drivetrain assembly and create custom fabricated parts.
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,28 @@
 var gulp = require('gulp');
-
+var imagemin = require('gulp-imagemin');
 var html5Lint = require('gulp-html5-lint');
- 
-gulp.task('html5-lint', function() {
-    return gulp.src('./public/*.html')
+
+var paths = {
+  images: 'static/img/**/*'
+};
+
+// Copy all static images 
+gulp.task('images', function() {
+  return gulp.src(paths.images)
+    // Pass in options to the task 
+    .pipe(imagemin({optimizationLevel: 5}))
+    .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('lint', function() {
+    return gulp.src('./public/**/*.html')
         .pipe(html5Lint());
 });
+
+// Rerun the task when a file changes 
+gulp.task('watch', function() {
+  gulp.watch(paths.images, ['images']);
+});
+
+// The default task (called when you run `gulp` from cli) 
+gulp.task('default', ['watch', 'lint', 'images']);
