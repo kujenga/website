054ebc2f5c38f0638411288e1093c9a121daaa40
kujenga <me@ataylor.io>
2019-09-05 22:56:32 -0400

    setup postinstall script for git worktree
    This is the cleanest method I have found so far for publishing
    to the gh-pages branch.

---
+++ scripts/postinstall.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This is the postinstall script for the repository, and it sets up a git
+# worktree for the gh-pages branch as outlined in this guide, it needed.
+# https://medium.com/linagora-engineering/deploying-your-js-app-to-github-pages-the-easy-way-or-not-1ef8c48424b7
+
+set -e
+
+if [ ! -f public/.git ]; then
+    git worktree add public gh-pages
+fi
