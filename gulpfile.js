8408a441c6e8038133806fe054df174a58fcad7e
kujenga <me@ataylor.io>
2019-09-07 09:23:21 -0400

    utilize file hashes for static assets
    This is a work-around for an issue where GitHub pages does not
    seem to immediately update CSS files when they change slightly.
    
    For my last site update, there was ~1 day of lag for styles.css
    to be updated, leaving it a bit broken.
    
    Some similar observations here:
    https://github.community/t5/GitHub-Pages/GitHub-Pages-doesn-t-use-updated-CSS-file/td-p/9985

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
-var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');
-var htmlhint = require("gulp-htmlhint");
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const htmlhint = require("gulp-htmlhint");
+const rev = require('gulp-rev');
+const revRewrite = require('gulp-rev-rewrite');
 
-var paths = {
+const paths = {
   images: 'static/img/**/*'
 };
+
+// Pre-processing
 
 // Copy all static images 
 function images() {
@@ -24,9 +28,37 @@ function watch() {
   gulp.watch(paths.images, ['images']);
 }
 
+// Post-processing
+
+// Revision the static assets to eliminate issues with GitHub pages not
+// updating files with small changes.
+function revision() {
+  return gulp.src('public/**/*.{css,js}')
+    .pipe(rev())
+    .pipe(gulp.dest('public'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('public'));
+}
+
+// Re-write HTML files to update references to the files that have had their
+// names changed to hashes.
+function rewrite() {
+  const manifest = gulp.src('public/rev-manifest.json');
+  return gulp.src('public/**/*.html')
+    .pipe(revRewrite({ manifest }))
+    .pipe(gulp.dest('public'));
+}
+
+// Tasks
+
 exports.images = images;
 exports.lint = lint;
 exports.watch = watch;
 
+// Pre-processing steps that formulate assets as input to Hugo.
+exports.pre = gulp.series(gulp.parallel(images, lint));
+// Post-processing steps that re-write assets built with Hugo.
+exports.post = gulp.series(revision, rewrite);
+
 // The default task (called when you run `gulp` from cli) 
-exports.default = gulp.series(gulp.parallel(images, lint));;
+exports.default = exports.pre;
