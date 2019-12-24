cfd10a4903cbcf5f5ec7a033e29fbab5ccf0f27e
Aaron Taylor <me@ataylor.io>
2019-12-23 23:52:23 -0500

    app: fancier local app, working appengine routing
    Resolves an issue where just content at the root was being served
    by app engine.

--- app/go.mod
+++ app/go.mod
@@ -1,3 +1,11 @@
 module github.com/kujenga/site/app
 
 go 1.12
+
+require (
+	go.uber.org/atomic v1.5.1 // indirect
+	go.uber.org/multierr v1.4.0 // indirect
+	go.uber.org/zap v1.13.0
+	golang.org/x/lint v0.0.0-20191125180803-fdd1cda4f05f // indirect
+	golang.org/x/tools v0.0.0-20191223235410-3721262b3e7c // indirect
+)
--- app/main.go
+++ app/main.go
@@ -1,9 +1,56 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"net/http"
+
+	"go.uber.org/zap"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	port := flag.Int("p", 8080, "port to listen on")
+	directory := flag.String("d", "./public", "static file directory to host")
+	flag.Parse()
+
+	logger, err := zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
+	(&server{
+		log:       logger,
+		port:      *port,
+		directory: *directory,
+	}).serve()
+}
+
+type server struct {
+	log       *zap.Logger
+	port      int
+	directory string
+}
+
+func (s *server) addr() string {
+	return fmt.Sprintf(":%d", s.port)
+}
+
+func (s *server) router() http.Handler {
+	mux := http.NewServeMux()
+
+	fs := http.FileServer(http.Dir(s.directory))
+	mux.Handle("/", fs)
+
+	return mux
+}
+
+func (s *server) serve() {
+	s.log.Info("Serving HTTP requests",
+		zap.Int("port", s.port),
+		zap.String("dir", s.directory),
+	)
+
+	err := http.ListenAndServe(s.addr(), s.router())
+	s.log.Fatal("error service http requests", zap.Error(err))
 }
