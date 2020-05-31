5cafa4dfef5ac5c689981a9ad27910d87a1a0146
Aaron Taylor <me@ataylor.io>
2020-05-31 13:56:19 -0400

    gulpfile: updates for app engine
    THis was left out of: https://github.com/kujenga/website/pull/1

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,33 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
-const htmlhint = require("gulp-htmlhint");
+const htmlhint = require('gulp-htmlhint');
 const rev = require('gulp-rev');
 const revRewrite = require('gulp-rev-rewrite');
 const revDel = require('gulp-rev-delete-original');
 
 const paths = {
-  images: 'static/img/**/*'
+  target: 'app/public',
+  images: 'static/img/**/*',
 };
 
 // Pre-processing
 
-// Copy all static images 
+// Copy all static images
 function images() {
-  return gulp.src(paths.images)
-    // Pass in options to the task 
-    .pipe(imagemin({optimizationLevel: 5}))
-    .pipe(gulp.dest('build/img'));
+  return (
+    gulp
+      .src(paths.images)
+      // Pass in options to the task
+      .pipe(imagemin({ optimizationLevel: 5 }))
+      .pipe(gulp.dest('build/img'))
+  );
 }
 
 function lint() {
-    return gulp.src('./public/**/*.html')
-        .pipe(htmlhint());
+  return gulp.src(`${paths.target}/**/*.html`).pipe(htmlhint());
 }
 
-// Rerun the task when a file changes 
+// Rerun the task when a file changes
 function watch() {
   gulp.watch(paths.images, ['images']);
 }
@@ -34,21 +37,23 @@ function watch() {
 // Revision the static assets to eliminate issues with GitHub pages not
 // updating files with small changes.
 function revision() {
-  return gulp.src('public/**/*.{css,js}')
+  return gulp
+    .src(`${paths.target}/**/*.{css,js}`)
     .pipe(rev())
     .pipe(revDel())
-    .pipe(gulp.dest('public'))
-    .pipe(rev.manifest())
-    .pipe(gulp.dest('public'));
+    .pipe(gulp.dest(paths.target))
+    .pipe(rev.manifest('rev-manifest.json'))
+    .pipe(gulp.dest(paths.target));
 }
 
 // Re-write HTML files to update references to the files that have had their
 // names changed to hashes.
 function rewrite() {
-  const manifest = gulp.src('public/rev-manifest.json');
-  return gulp.src('public/**/*.html')
+  const manifest = gulp.src(`${paths.target}/rev-manifest.json`);
+  return gulp
+    .src(`${paths.target}/**/*.html`)
     .pipe(revRewrite({ manifest }))
-    .pipe(gulp.dest('public'));
+    .pipe(gulp.dest(paths.target));
 }
 
 // Tasks
@@ -62,5 +67,5 @@ exports.pre = gulp.series(gulp.parallel(images, lint));
 // Post-processing steps that re-write assets built with Hugo.
 exports.post = gulp.series(revision, rewrite);
 
-// The default task (called when you run `gulp` from cli) 
+// The default task (called when you run `gulp` from cli)
 exports.default = exports.pre;
