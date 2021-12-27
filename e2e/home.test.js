b21ae1648a2c915fcbca0210c961e01a5799da90
Aaron Taylor <git@ataylor.io>
2021-12-26 22:39:01 -0500

    e2e: add test for search query, blog assets
    This has the side benefit of asserting that the javascript is being
    properly loaded into the webpages, which would have handled the past
    regression with missing static assets.

--- e2e/home.test.js
+++ e2e/home.test.js
@@ -29,4 +29,31 @@ describe('Homepage', () => {
   it('should be titled with my name', async () => {
     await expect(page.title()).resolves.toContain('Aaron Taylor');
   });
+
+  it('should properly execute search queries', async () => {
+    // Enter a text query and submit it.
+    await page.evaluate(
+      () =>
+        (document.querySelector('input[name=query]').value = 'neural network')
+    );
+    await page.evaluate(() =>
+      document.querySelector('button[type="submit"]').click()
+    );
+    // Wait for results and check that there is at least one of them.
+    await page.waitForSelector('div#results');
+    await page.waitForSelector('li > div.summary');
+  });
+
+  it('should properly load blog assets', async () => {
+    // Navigate to the URL for a blog post with all assets.
+    await page.evaluate(() => (window.location.pathname = '/blog/go-mlp/'));
+    // Wait for main blog div to appear.
+    await page.waitForSelector('div.type-blog');
+    // Wait for mathjax rendering to appear.
+    await page.waitForSelector('mjx-math');
+    // Wait for emgithub to appear.
+    await page.waitForSelector('div.emgithub-container');
+    // Wait for giscus comments to appear.
+    await page.waitForSelector('iframe.giscus-frame');
+  });
 });
