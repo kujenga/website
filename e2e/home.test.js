56087e7fe47b4c0819a782c881f4b4168526c9fe
Aaron Taylor <git@ataylor.io>
2021-12-26 21:40:11 -0500

    circleci,e2e: run e2e tests in CI against latest deployed version

--- e2e/home.test.js
+++ e2e/home.test.js
@@ -1,8 +1,29 @@
 import 'expect-puppeteer';
+import { existsSync, readFileSync } from 'fs';
+
+function getTargetURL() {
+  if (process.env.TARGET_URL) {
+    // IF a TARGET_URL env var is specified, utilize that first.
+    return process.env.TARGET_URL.trim();
+  }
+  if (existsSync('./VERSION_URL')) {
+    // Read the VERSION_URL from disk based on the output of the
+    // deploy-appengine script, executed in CI environments.
+    return readFileSync('./VERSION_URL', 'utf8').trim();
+  }
+  // In CI environments, we want to make sure that one of the above options is
+  // used, since there is little purpose testing the default application.
+  if (process.env.CI) {
+    throw new Error('must have explicit version in CI');
+  }
+  return 'https://ataylor.io';
+}
 
 describe('Homepage', () => {
   beforeAll(async () => {
-    await page.goto('https://ataylor.io');
+    const targetURL = getTargetURL();
+    console.log('testing at:', targetURL);
+    await page.goto(targetURL);
   });
 
   it('should be titled with my name', async () => {
