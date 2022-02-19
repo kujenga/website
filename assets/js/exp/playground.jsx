b93a6c5a9e8cffd88212c55afde8cc900228d4ae
Aaron Taylor <git@ataylor.io>
2022-02-18 21:49:33 -0500

    exp/playground: add toggle for auto-rendering
    This allows for different usage patterns.

--- assets/js/exp/playground.jsx
+++ assets/js/exp/playground.jsx
@@ -2,36 +2,59 @@
 
 import { h, Component, render } from 'preact';
 
+const Defaults = {
+  template: 'Hello, {{ .Name }}!',
+  data: JSON.stringify({ Name: 'World' }, null, '  '),
+  autoRender: true,
+};
+
 /**
  * Playground provides a template rendering capability for Go templates.
  */
 class Playground extends Component {
   constructor() {
     super();
-    this.state = {
-      template: 'Hello, {{ .Name }}!',
-      data: JSON.stringify({ Name: 'World' }, null, '    '),
-    };
-    this.state.rendered = ExpRenderGoTemplate(
-      this.state.template,
-      this.state.data
-    );
+    this.state = this.newState({}, Defaults, true);
+  }
+
+  /**
+   * newState augments the passed in state values with rendering logic.
+   *
+   * @param {object} prev - Previous state values.
+   * @param {object} s - New state values.
+   * @param {boolean} forceRender - Set to true to force rendering.
+   * @returns {object} - Complete new state value to pass to setState.
+   */
+  newState(prev, s, forceRender = false) {
+    if (forceRender || this.state.autoRender) {
+      s.rendered = ExpRenderGoTemplate(
+        s.template || prev.template,
+        s.data || prev.data
+      );
+    }
+    return Object.assign(prev, s);
   }
 
   updateTemplate = (e) => {
-    this.setState((prev) =>
-      Object.assign(prev, {
-        template: e.target.value,
-        rendered: ExpRenderGoTemplate(e.target.value, prev.data),
-      })
-    );
+    this.setState((prev) => this.newState(prev, { template: e.target.value }));
   };
 
   updateData = (e) => {
+    this.setState((prev) => this.newState(prev, { data: e.target.value }));
+  };
+
+  updateRendered = () => {
+    this.setState((prev) => this.newState(prev, {}, true));
+  };
+
+  setDefaults = () => {
+    this.setState((prev) => this.newState(prev, Defaults, true));
+  };
+
+  toggleAutoRender = () => {
     this.setState((prev) =>
       Object.assign(prev, {
-        data: e.target.value,
-        rendered: ExpRenderGoTemplate(prev.template, e.target.value),
+        autoRender: !this.state.autoRender,
       })
     );
   };
@@ -43,22 +66,22 @@ class Playground extends Component {
           <div class="form-group">
             <label for="templateTextArea">Template</label>
             <textarea
-              class="form-control"
+              class="form-control mono"
               id="templateTextArea"
               value={state.template}
               onInput={this.updateTemplate}
-              rows="8"
+              rows="12"
               autocomplete="off"
             />
           </div>
           <div class="form-group">
             <label for="dataTextArea">Data (JSON)</label>
             <textarea
-              class="form-control"
+              class="form-control mono"
               id="dataTextArea"
               value={state.data}
               onInput={this.updateData}
-              rows="5"
+              rows="6"
               autocomplete="off"
             />
           </div>
@@ -67,12 +90,46 @@ class Playground extends Component {
           <div class="form-group">
             <label for="renderTextArea">Rendered</label>
             <textarea
-              class="form-control"
+              class="form-control mono"
               id="renderTextArea"
               value={state.rendered}
-              rows="16"
+              rows="12"
               disabled="true"
             />
+          </div>
+          <div class="form-group">
+            <label>Configuration</label>
+            <div class="form-inline">
+              <button
+                type="submit"
+                class="btn btn-primary"
+                disabled={state.autoRender}
+                onClick={this.updateRendered}
+              >
+                Render
+              </button>
+              <div class="custom-control custom-switch ml-3">
+                <input
+                  type="checkbox"
+                  class="custom-control-input"
+                  id="autoRender"
+                  checked={state.autoRender}
+                  onClick={this.toggleAutoRender}
+                />
+                <label class="custom-control-label" for="autoRender">
+                  Auto-render
+                </label>
+              </div>
+            </div>
+            <div class="form-inline mt-2">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                onClick={this.setDefaults}
+              >
+                Restore Defaults
+              </button>
+            </div>
           </div>
         </div>
       </div>
