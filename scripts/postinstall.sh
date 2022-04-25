69bbfa9de45f42b0f0be860669b6fa83af5828b5
Aaron Taylor <git@ataylor.io>
2022-04-25 07:49:52 -0400

    update README for new installs
    Helpful for anyone wanting to try out the site or mess around.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,13 +2,18 @@
 
 set -e
 
+# Post-install steps
+set -x
+
 # NOTE: As we are currently using App Engine to deploy and serve this site,
 # this is disabled at the current time.
-
+#
 # This is the postinstall script for the repository, and it sets up a git
 # worktree for the gh-pages branch as outlined in this guide, it needed.
 # https://medium.com/linagora-engineering/deploying-your-js-app-to-github-pages-the-easy-way-or-not-1ef8c48424b7
-
 # if [ ! -f public/.git ]; then
 #     git worktree add public gh-pages
 # fi
+
+# Ensure git submodules are installed:
+git submodule update --init --recursive
